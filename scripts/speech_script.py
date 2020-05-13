9562a9fa457ccfa3f01e4de6404d057fec49f4ef
jefer-a <717691356@qq.com>
2020-05-13 20:46:32 +0800

    insert speech_scrtpt and alter launch

--- scripts/speech_script.py
+++ scripts/speech_script.py
@@ -5,13 +5,12 @@
 import os,sys
 import time
 
-def select():
+def select(txtFile):
     while True:
         print("请选择发布模式：")
         print("1.自动延时逐条发布")
         print("2.手动延迟逐条发布")
         print("3.手动选择单条发布")
-        txtFile=open("speech.txt",'r',encoding="utf-8")
         option=input()
         if  '1' in option:
             publisher_one(txtFile)
@@ -33,54 +32,51 @@ def publisher_one(txtFile):
     for commod in txtFile.readlines():
         if commod[0]=='#':
             continue
-        os.system("rostopic pub -1 /voice_txt /sdt_message/string "+commod)
-        time.sleep(15)
-        print(commod)
+        cmd_pub(commod,15)
 
 def publisher_two(txtFile):
     for commod in txtFile.readlines():
-        # print(commod)
-        if commod[0]=='#':
-            continue
-        os.system("rostopic pub -1 /voice_txt /sdt_message/string "+commod)
-        time.sleep(1)
-        print("发布下一条指令请回车！")
+        cmd_pub(commod)
+        print("回车发布下一条指令！")
         input()
 
 def publisher_three(txtFile):
     i=0
-    count=0
     commods=txtFile.readlines()
+    print("请选择路径控制指令编号(退出输入[n]):")
     for commod in commods:
         i+=1
         print(str(i)+"："+commod)
-    print("请选择欲发布的路径控制指令编号(退出输入[n]):")
     while True:
-        count+=1
-        if count >10:
-            for commod in txtFile.readlines():
-                i+=1
-                print(str(i)+"："+commod)
-            count=0
         select_commod=input()
         try:
             if 0 < int(select_commod) <= i:
                 commod=commods[int(select_commod)-1]
-                os.system("rostopic pub -1 /voice_txt /sdt_message/string "+commod)
-                time.sleep(1)
+                cmd_pub(commod)
+                print("请选择路径控制指令编号(退出输入[n]):")
+                continue
         except :
+        	pass
+        try:
             if select_commod=='n':
                 print("退出模式3!")
                 break
-            else:
-                print('无效输入，请再次输入!')
-                continue
-        print("请选择欲发布的路径控制指令编号(退出输入[n]):")
+        except:
+            print('无效编号，请再次输入!')
+
+def cmd_pub(commod,delay=1):
+	print(commod)
+	cmd="rostopic pub -1 /voice_txt std_msgs/String "+"\""+commod+"\""
+	os.system("gnome-terminal -e "+"\'"+cmd+"\'")
+	time.sleep(delay)
 
 
 
 if __name__ == "__main__":
-    if os.path.isfile("speech.txt"):
-        select()
-    else:
+    try:
+        script_path=os.path.dirname(__file__)
+        txt_path=script_path+'/speech.txt'
+        txtFile=open(txt_path,'r',encoding="utf-8")
+        select(txtFile)
+    except:
         print("speech.txt文件不存在")
